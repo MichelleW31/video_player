31fde8cb59dd5568666b050d5d736c181a767c82
Michelle Williams <mmwilliams@C02XW60FJHD2.attlocal.net>
2019-10-20 18:52:28 -0400

    build header component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,75 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    padding : 0;
+    margin: 0;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-rendering: optimizeLegibility;
+  }
+
+  html{
+    font-size: 2.5vw;
+  }
+
+  @font-face {
+    font-family: 'BrandonRegular';
+    src: url("assets/fonts/BRANDON_REG.otf") format("opentype");
+    font-weight: normal;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'BrandonLight';
+    src: url("assets/fonts/BRANDON_LIGHT.otf") format("opentype");
+    font-weight: normal;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'BrandonMedium';
+    src: url("assets/fonts/BRANDON_MED.otf") format("opentype");
+    font-weight: normal;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'BrandonBold';
+    src: url("assets/fonts/BRANDON_BLD.otf") format("opentype");
+    font-weight: normal;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'BrandonBlack';
+    src: url("assets/fonts/BRANDON_BLK.otf") format("opentype");
+    font-weight: normal;
+    font-style: normal;
+  }
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin: 0;
+    padding: 0;
+  }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="header">
+    <h2 class="header-title">Welcome, <span class="user">Michelle</span></h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+  // props: {
+  //
+  // }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .header {
+    background: #009ddb;
+    font-family: 'BrandonLight', Arial, sans-serif;
+    padding: 70px 50px;
+  }
+
+  .header-title {
+    color: white;
+    font-weight: 100;
+    font-size: 2rem;
+    text-align: left;
+  }
+
+  .user{
+    font-family: BrandonRegular, Arial, sans-serif;
+  }
+</style>

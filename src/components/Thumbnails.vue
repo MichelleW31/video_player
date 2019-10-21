0339c1f69e4ae112c1c98ff4c6c2897ff07d727b
Michelle Williams <mmwilliams@C02XW60FJHD2.attlocal.net>
2019-10-20 22:14:18 -0400

    thumbnails showing

--- src/components/Thumbnails.vue
+++ src/components/Thumbnails.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="thumbnails">
     <div v-for="video in videos" :key="video.path">
-      {{video.path}}
+      <img :src="require(`../assets/images/${video.path}.jpg`)" :alt="video.path">
     </div>
   </div>
 </template>
